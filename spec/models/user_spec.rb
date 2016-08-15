b25d5b795408670278f632dde984add2fb8d1167
ichai <nahy@ya.ru>
2016-08-16 01:54:15 +0300

    User to_s test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,9 @@ RSpec.describe User, type: :model do
   it 'has a valid factory' do
     expect(FactoryGirl.build(:user)).to be_valid
   end
+
+  it 'should return email when to_s' do
+    user = FactoryGirl.create(:user)
+    expect(user.to_s).to eq(user.email)
+  end
 end
