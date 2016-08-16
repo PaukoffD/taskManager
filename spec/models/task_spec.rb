cbc9500fceed028a2a0c6d9d9f1caff596422854
ichai <nahy@ya.ru>
2016-08-16 12:11:01 +0300

    Models spec

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe Task, type: :model do
   it 'has a valid factory' do
     expect(FactoryGirl.build(:task)).to be_valid
   end
+
+  it { is_expected.to belong_to(:user) }
+  it { is_expected.to validate_presence_of(:user) }
+  it { is_expected.to validate_presence_of(:name) }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,4 +9,12 @@ RSpec.describe User, type: :model do
     user = FactoryGirl.create(:user)
     expect(user.to_s).to eq(user.email)
   end
+
+  it { is_expected.to have_many(:tasks) }
+
+  it { is_expected.to validate_presence_of(:email) }
+  it { is_expected.to validate_uniqueness_of(:email) }
+  it { is_expected.to validate_length_of(:email).is_at_least(4).is_at_most(254) }
+
+  it { is_expected.to have_secure_password }
 end
