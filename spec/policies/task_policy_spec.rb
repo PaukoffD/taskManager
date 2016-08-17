b6208354d7bbcfbaebbab59418b0602ff91cf304
ichai <nahy@ya.ru>
2016-08-17 14:05:29 +0300

    Remove not ready spec

--- spec/policies/task_policy_spec.rb
+++ spec/policies/task_policy_spec.rb
@@ -3,10 +3,6 @@ require 'rails_helper'
 RSpec.describe TaskPolicy do
   subject { described_class.new(user, task) }
 
-  let(:resolved_scope) do
-    described_class::Scope.new(user, Task.all).resolve
-  end
-
   context 'user is admin' do
     let(:user) { create(:user, :admin) }
     let(:task) { create(:task, user: user)}
